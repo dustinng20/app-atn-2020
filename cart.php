7078c46ad2711bc0ece7d41c0176026b47206afe
dunghk123 <58968686+dunghk123@users.noreply.github.com>
2020-04-27 22:44:26 +0700

    use pgadmin

--- cart.php
+++ cart.php
@@ -15,7 +15,7 @@
 
     // Check connection
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         insertToDB($conn);
